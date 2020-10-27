ec276eed5ec1b3f3e7b666165084f5a7e55e0aac
Zhu Mei <meizhu812@outlook.com>
2020-10-27 20:27:57 +0800

    [Zhu Mei]add Trainee

---
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/Trainee.java
@@ -0,0 +1,27 @@
+package com.thoughtworks.capability.gtb.entrancequiz;
+
+public class Trainee {
+    private Integer id;
+    private String name;
+
+    public Trainee(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
